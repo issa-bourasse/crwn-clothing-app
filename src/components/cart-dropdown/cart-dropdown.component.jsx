e8175bbb7eb8ff4a4c2f3a9e0511563b9015f824
Issa Bourasse <bourasseissa@gmail.com>
2024-11-20 10:04:49 +0100

    finish the route to all the clothing pages

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -18,8 +18,14 @@ const CartDropdown = () => {
     return (
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
-                {cartItems.map((item) => ( 
-                    <CartItem key={item.id} cartItem={item} />))}
+                {cartItems.length ? (
+                    cartItems.map((item) => (
+                        <CartItem key={item.id} cartItem={item} />
+                    ))
+                ) : (
+                    <span className='empty-message'>Your cart is empty</span>
+                )}
+                
             </div>
             <Button buttonType='inverted' onClick={goToCheckoutHandler}>Go to checkout</Button>
         </div>
--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./directory-item.style.scss";
 
 
 const DirectoryItem = ({ category }) => {
-    const {imageUrl, title} = category;
+    const {imageUrl, title , route} = category;
+    const navigate = useNavigate();
+
+    const onNavigateHandler = () => {
+        navigate(route);
+    }
+
   return (
     <div className="directory-item-container">
       <div
@@ -12,7 +19,7 @@ const DirectoryItem = ({ category }) => {
           backgroundImage: `url(${imageUrl})`,
         }}
       />
-      <div className="body">
+      <div className="body" onClick={onNavigateHandler}>
         <h2>{title}</h2>
         <p>Shop Now</p>
       </div>
