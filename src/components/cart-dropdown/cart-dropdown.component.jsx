04be31ae9af81b1120c180d9ceb3d4202d86bc70
Issa Bourasse <bourasseissa@gmail.com>
2024-11-27 15:39:42 +0100

    close the carte whene i click outside

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { CartContext } from '../../context/cart.context';
@@ -7,16 +7,29 @@ import CartItem from '../cart-item/cart-item.component';
 import './cart-dropdown.styles.scss';
 
 const CartDropdown = () => {
-
-    const {cartItems} = useContext(CartContext);
+    const { cartItems, setIsCartOpen } = useContext(CartContext);
     const navigate = useNavigate();
+    const dropdownRef = useRef(null);
 
     const goToCheckoutHandler = () => {
         navigate('/checkout');
-    }
+    };
+
+    const handleClickOutside = (event) => {
+        if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+            setIsCartOpen(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
 
     return (
-        <div className='cart-dropdown-container'>
+        <div className='cart-dropdown-container' ref={dropdownRef}>
             <div className='cart-items'>
                 {cartItems.length ? (
                     cartItems.map((item) => (
@@ -25,11 +38,10 @@ const CartDropdown = () => {
                 ) : (
                     <span className='empty-message'>Your cart is empty</span>
                 )}
-                
             </div>
             <Button buttonType='inverted' onClick={goToCheckoutHandler}>Go to checkout</Button>
         </div>
     );
-}
+};
 
 export default CartDropdown;
